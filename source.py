ffc5a42040c734efff90daa9e8a44f4c8540f8eb
sui <1202ayasnppk@gmail.com>
2023-04-01 09:22:31 +0900

    📚 Reply to negative words

--- source.py
+++ source.py
@@ -1,14 +1,18 @@
+import os
 import discord
 
 # Bot のアクセストークン
-DISCORD_BOT_TOKEN = "TOKEN"
+DISCORD_BOT_TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
 
 # 接続に必要な設定/オブジェクト
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.typing = False
 intents.presences = False
 
 client = discord.Client(intents=intents)
+
+# ネガティブな単語のリスト
+negative_words = ["悲しい", "辛い", "もうだめだ"]
 
 # 起動時に動作する処理
 @client.event
@@ -20,16 +24,19 @@ async def on_ready():
 @client.event
 async def on_message(message):
     # Bot からのメッセージは無視する
-    if message.author.bot:
+    if message.author == client.user:
         return
 
     # Hello と送られたら Hi! と返す
-    if message.content == "Hello":
+    if message.content.lower() == "hello":
         await message.channel.send("Hi!")
 
-    if message.content == "/sco":
-        await message.channel.send("Hi!")
+    # ネガティブな単語がメッセージに含まれているかチェックする
+    for word in negative_words:
+        if word in message.content:
+            # ネガティブな単語が含まれている場合、返信を送信する
+            await message.channel.send("ピピーっ！👮👮ネガティブ警察です🚨🚨🚨🙅🙅🙅🙅\nそのツイートは❗❗❗ネガティブな考えになるゾ😤😤😤💢💢💢")
+            break
 
 # Bot の起動と Discord サーバーへの接続
 client.run(DISCORD_BOT_TOKEN)
-
